bf6f8c750ca9f72cf58bae108ead05d94a2d3159
vrcjio <132543591+vrcjio@users.noreply.github.com>
2023-05-26 11:04:16 +0530

    first

--- next.config.js
+++ next.config.js
@@ -2,3 +2,14 @@
 const nextConfig = {}
 
 module.exports = nextConfig
+
+module.exports = {
+    async rewrites() {
+      return [
+        {
+          source: '/:path*',
+          destination: 'https://web.dev.adeemoo.com/api/v0.1/:path*',
+        },
+      ];
+    },
+  };
---
+++ src/app/singup/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const singup = () => {
+  return (
+    <div>singup</div>
+  )
+}
+
+export default singup
